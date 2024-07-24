723a1d1ab0331b7e565ea02c90bc3b247943ecc2
KaysonSouksaly <200577143@student.georgianc.on.ca>
2024-07-23 21:14:13 -0400

    Advertisement Commit
    Change location of Ad

--- universalElement/advertisement.js
+++ universalElement/advertisement.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const adHTML = `
         <section class="ad">
-            <a class="display-item box-1 visible" href="event.php">
+            <a class="display-item box-1 visible" href="event.html">
                 <img src="./Imgs/ad-1.png" alt="Ad Posting">
                 <span class="hover-effect">Click Me</span>
             </a>
-            <a class="display-item box-2" href="event.php">
+            <a class="display-item box-2" href="event.html">
                 <img src="./Imgs/ad-2.png" alt="Ad Posting">
                 <span class="hover-effect">Click Me</span>
             </a>
-            <a class="display-item box-3" href="event.php">
+            <a class="display-item box-3" href="event.html">
                 <img src="./Imgs/ad-3.png" alt="Ad Posting">
                 <span class="hover-effect">Click Me</span>
             </a>
         </section>
     `;
 
-    document.body.insertAdjacentHTML('afterbegin', adHTML);
+    const adPlaceholder = document.getElementById('ad-placeholder');
+    adPlaceholder.innerHTML = adHTML;
 });
